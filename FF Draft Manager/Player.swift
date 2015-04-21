768b40639f001eff994133556da95f2f0c307a04
J. Brant Carson <bcarson408@gmail.com>
2015-04-20 23:28:49 -0700

    test

---
+++ FF Draft Manager/Player.swift
@@ -0,0 +1,18 @@
+//
+//  Player.swift
+//  FF Draft Manager
+//
+//  Created by J. Brant Carson on 4/20/15.
+//  Copyright (c) 2015 J. Brant Carson. All rights reserved.
+//
+
+import Foundation
+
+
+Class Player {
+    var first_name:String?;
+    var last_name:string?;
+    var team:String?;
+    var position:string?;
+    
+}
